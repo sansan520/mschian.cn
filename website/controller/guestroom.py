878da3f4b5fbe092bddae518022023a950324dc4
JsonWang <632494788@qq.com>
2016-08-19 17:16:38 +0800

    添加客房详细

--- website/controller/guestroom.py
+++ website/controller/guestroom.py
@@ -58,6 +58,8 @@ def do_insert_guestroom():
     gr_room_area = request.get_json().get("gr_room_area")
     gr_bed_type = request.get_json().get("gr_bed_type")
     gr_bed_count = request.get_json().get("gr_bed_count")
+    gr_windows = request.get_json().get("gr_windows")
+    gr_breakfast = request.get_json().get("gr_breakfast")
     gr_settings = request.get_json().get("gr_settings")
 
     data = json.dumps({
@@ -71,7 +73,9 @@ def do_insert_guestroom():
         'gr_room_area': gr_room_area,
         'gr_bed_type': gr_bed_type,
         'gr_bed_count': gr_bed_count,
-        'gr_settings': gr_settings,
+        'gr_windows': gr_windows,
+        'gr_breakfast': gr_breakfast,
+        'gr_settings': gr_settings
     })
     response = requests.post(url=api+'/api/v1.0/gr_insert',
                              data=data,
