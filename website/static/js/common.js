e57f36f782106a1089908a5f0dff034374b9971c
JsonWang <632494788@qq.com>
2016-10-14 21:01:16 +0800

    登录登出JS修改

--- website/static/js/common.js
+++ website/static/js/common.js
@@ -2,11 +2,31 @@ $(document).ready(function(){
 
     dropdownOpen();//index 调用
 
-    
-    
-
 });
 
+function login_out(){
+    $.ajax({
+            url:"/do_logout",
+            type: 'POST',
+            contentType: 'application/json',
+            success: function(data){
+                alert(data.message);
+                // if(data.code==0){
+                //     alert(data.message); //登出异常
+                // }else if(data.code==1){  //==1 登录成功
+                //
+                //     // setTimeout('jumpUrl("/index")',2000);
+                //     //  layer.msg(data.message, {
+                //     //     time: 2000, //2s后自动关闭
+                //     //     btn: ['确定']
+                //     //  });
+                // }
+            }
+        });
+}
+function jumpUrl(pageUrl) {
+    location.href=pageUrl;
+}
 
 function ms_login_check_name() {
     var u_name = $(".username-input").val();
